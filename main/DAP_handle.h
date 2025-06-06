03838c315c21538a7430dd27b7e9d5d4c09ca36d
wkwzms <123885293+wkwzms@users.noreply.github.com>
2025-06-06 13:56:46 +0800

    Update DAP_handle.h

--- main/DAP_handle.h
+++ main/DAP_handle.h
@@ -14,6 +14,5 @@ void handle_dap_data_request(usbip_stage2_header *header, uint32_t length);
 void handle_swo_trace_response(usbip_stage2_header *header);
 void handle_dap_unlink();
 
-int fast_reply(uint8_t *buf, uint32_t length, int dap_req_num);
-
+int fast_reply(uint8_t *buf, uint32_t length);
 #endif
