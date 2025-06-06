5033db7e8769806c8b5747ce0f93af890e07e175
wkwzms <123885293+wkwzms@users.noreply.github.com>
2025-06-06 13:42:52 +0800

    Update usbip_server.h

--- main/usbip_server.h
+++ main/usbip_server.h
@@ -12,12 +12,23 @@ enum usbip_server_state_t
     WAIT_URB,
 };
 
-extern int kSock;
+
 
 int usbip_worker(uint8_t *base, uint32_t length, enum usbip_server_state_t *state);
 void send_stage2_submit_data(usbip_stage2_header *req_header, int32_t status, const void * const data, int32_t data_length);
 void send_stage2_submit(usbip_stage2_header *req_header, int32_t status, int32_t data_length);
 void send_stage2_submit_data_fast(usbip_stage2_header *req_header, const void *const data, int32_t data_length);
 int usbip_network_send(int s, const void *dataptr, size_t size, int flags);
- int attach(uint8_t *buffer, uint32_t length);
+enum state_t
+{
+    ACCEPTING,
+    ATTACHING,
+    EMULATING,
+    EL_DATA_PHASE
+};
+extern uint8_t kState;
+extern int kSock;
+
+int attach(uint8_t *buffer, uint32_t length);
+int emulate(uint8_t *buffer, uint32_t length);
 #endif
